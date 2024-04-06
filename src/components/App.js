914bca7f4ba86e9cd9e656ce0bf84b5366e76d06
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-04-06 07:30:51 +0000

    Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,23 @@
 
 import React from "react";
 import './../styles/App.css';
-import { useState } from "react";
-
+import React, { useState } from "react";
 
 function App() {
-  const [change, setChange] = useState();
+  const [change, setChange] = useState("");
+
   function makeChange(e) {
     setChange(e.target.value);
   }
+
   return (
     <div className="App">
-      <label>Enter your name:</label>
+      <label>Enter your name</label>
       <input 
-      value={change}
-      onChange={makeChange}
+        value={change}
+        onChange={makeChange} // Pass the event object to makeChange
       />
-      {change.trim() !== "" ? <p>Hello, {change}!</p> : null}
+      {change.trim() !== "" ? <p>Hello {change}!</p> : null}
     </div>
   );
 }
