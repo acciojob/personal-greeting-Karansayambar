57a9fd9fb2c00738da5ff24a1212552ee6c255b5
Karan Sayambar <86702398+Karansayambar@users.noreply.github.com>
2024-04-06 07:22:55 +0000

    code is added

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,19 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [change, setChange] = useState("");
+  function handleChange(e){
+    setChange(e.target.value);
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <label>Enter your name</label>
+        <input
+        value={change}
+        onChange ={handleChange()}
+        />
+        {change.trim() !== "" ? <p>Hello {change}!</p> : null}
     </div>
   )
 }
